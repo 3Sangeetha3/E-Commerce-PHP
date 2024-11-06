ec6678af4863f3ba357462e2c7c19a2297ed849b
Jadamal Sangeetha Choudhary <jadamalsangeetha@lpu.in>
2024-11-06 23:29:25 +0530

    Update footer links to use absolute paths for About Us and Contact Us pages

--- components/footer.php
+++ components/footer.php
@@ -11,10 +11,10 @@
                 <ul class="list-unstyled mb-0">
                     <li><a href="../index.php" class="text-light">Home</a></li>
                     <li><a href="#" class="text-light">Shop</a></li>
-                    <li><a href="../pages/AboutUs.php" class="text-light">Shop</a></li>
-                    <li><a href="#" class="text-light">Contact Us</a></li>
-                    <li><a href="../pages/signIn.php" class="text-light">Sign In</a></li>
-                    <li><a href="../pages/signUp.php" class="text-light">Sign Up</a></li>
+                    <li><a href="/E-commerce/pages/AboutUs.php" class="text-light">About Us</a></li>
+                    <li><a href="/E-commerce/pages/contact.php" class="text-light">Contact Us</a></li>
+                    <li><a href="/E-commerce/pages/signIn.php" class="text-light">Sign In</a></li>
+                    <li><a href="/E-commerce/pages/signUp.php" class="text-light">Sign Up</a></li>
                 </ul>
             </div>
             <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
