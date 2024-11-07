12aa7b709957ce246751e7a218efcee06774d35b
Jadamal Sangeetha Choudhary <jadamalsangeetha@lpu.in>
2024-11-07 12:49:01 +0530

    Enhance product image with animation effect on index page

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
                 <a href="#" id="shopNowButton" class="btn text-light animate__animated animate__jello" style="background-color: #FF7F50;">Shop Now</a>
             </div>
             <div class="col-md-6 text-center">
-                <img src="assets/images/image1.svg" class="img-fluid" alt="Leather Products">
+                <img src="assets/images/image1.svg" class="img-fluid animate__animated animate__bounceInDown" alt="Leather Products">
             </div>
         </div>
     </div>
